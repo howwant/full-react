c1fa3335d7ee4184f197bbb1e4eaf9ee9d5eee7c
kanghwawon <corn1128@naver.com>
2023-04-16 14:14:42 +0900

    무한 스크롤 기능

--- client/components/MsgList.js
+++ client/components/MsgList.js
@@ -1,35 +1,23 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useRouter } from "next/router"
 import MsgItem from "./MsgItem"
 import MsgInput from "./MsgInput"
 import fetcher from '../fetcher'
+import useInfinteScroll from "../hooks/useInfiniteScroll"
 
-// const UserId = ['roy', 'jay']
-// const getRandomUserId = () => UserId[Math.round(Math.random())]
-// const originalMsgs = Array(50).fill(0).map((_,i) => ({
-//     id: 50 - i,
-//     userId: getRandomUserId(),
-//     timestamp: 1234567890123 + (50 - i) * 1000 * 60,
-//     text: `${50 - i} mock tex t`
-// }))
 const MsgList = () => {
     const { query } = useRouter()
     const userId = query.userId || query.userid || ''; // userid 대응
     const [ msgs, setMsgs ] = useState([])
     const [ editingId, setEditingId ] = useState(null)
+    const [hasNext, setHasNext] = useState(true)
+    const fetchMoreEl = useRef(null)
+    const intersecting = useInfinteScroll(fetchMoreEl)
 
     const onCreate = async text => {
         const newMsg = await fetcher('post', '/messages', {text, userId})
         if (!newMsg) throw Error('something wrong')
         setMsgs(msgs => [newMsg, ...msgs])
-
-        // const newMsg = {
-        //     id: msgs.length + 1,
-        //     userId: getRandomUserId(),
-        //     timestamp: Date.now(),
-        //     text:`${msgs.length + 1} ${text}`,
-        // }
-        // console.log(msgs)
     }
     
     const onUpdate = async(text, id) => {
@@ -59,30 +47,37 @@ const MsgList = () => {
     const doneEdit = () => setEditingId(null)
 
     const getMessages = async() => {
-        const msgs = await fetcher('get', '/messages')
-        setMsgs(msgs)
+        const newMsgs = await fetcher('get', '/messages',{ params: { cursor: msgs[msgs.length -1]?.id || ''}})
+        if(newMsgs.length === 0){
+            setHasNext(false)
+            return
+        }
+        setMsgs(msgs => [...msgs,...newMsgs])
     }
 
     useEffect(() => {
-        getMessages()
-    },[])
+        if(intersecting && hasNext) getMessages()
+    },[intersecting])
 
     return (
-        <>{userId &&
-        <MsgInput mutate={onCreate}/>}
-        <ul className="messages">
-        { msgs.map(x => 
-        <MsgItem 
-        key={x.id}{ ...x } 
-        onUpdate={onUpdate} 
-        startEdit={() => setEditingId(x.id)} 
-        isEditing={editingId === x.id}
-        onDelete={() => onDelete(x.id)}
-        myId={userId}
-        />
-        )}
+    <>
+        {userId &&
+            <MsgInput mutate={onCreate}/>}
+           <ul className="messages">
+            { msgs.map(x => 
+            <MsgItem 
+            key={x.id}{ ...x } 
+            onUpdate={onUpdate} 
+            startEdit={() => setEditingId(x.id)} 
+            isEditing={editingId === x.id}
+            onDelete={() => onDelete(x.id)}
+            myId={userId}
+            />
+            )}
         </ul>
-        </>
+        <div ref={fetchMoreEl}/>
+    </>
+
     )
 }
 
